1edbf80752bd83c1c93940d3541a75fd3ee3960c
Igor Siergiej <igorsiergiej@gmail.com>
2023-07-30 22:55:07 +0100

    Started working on displaying all exercises screens

--- app/src/main/java/com/example/setrep/model/Workout.kt
+++ app/src/main/java/com/example/setrep/model/Workout.kt
@@ -1,6 +1,9 @@
 package com.example.setrep.model
 
+import java.time.LocalDateTime
+
 class Workout {
     var timeTaken = 0
+    var date = LocalDateTime.now()
     var exercises = ArrayList<Exercise>()
 }
--- app/src/main/java/com/example/setrep/views/CalendarScreen.kt
+++ app/src/main/java/com/example/setrep/views/CalendarScreen.kt
@@ -2,6 +2,10 @@ package com.example.setrep.views
 
 import android.annotation.SuppressLint
 import android.icu.text.SimpleDateFormat
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,16 +14,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.DatePickerState
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
+import androidx.compose.material3.TabPosition
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -28,10 +39,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.setrep.datasource.ProfileViewModel
+import com.example.setrep.ui.components.button.SegmentedControl
 import com.example.setrep.ui.components.scaffold.MainScaffold
 import com.example.setrep.ui.components.topbar.EmptyTopBar
 import java.util.Calendar
@@ -121,9 +135,66 @@ private fun CalendarPickerScreen() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun SortExercisesScreen() {
+    val listItems = arrayOf("Date", "Time Taken", "Test")
 
+    var expanded by remember { mutableStateOf(false) }
+
+    var selectedItem by remember { mutableStateOf(listItems[0]) }
+
+    val titles = listOf("Ascending", "Descending")
+
+    var selectedOrder by remember { mutableStateOf(titles[0]) }
+
+    Column(
+        modifier = Modifier.padding(10.dp).fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ExposedDropdownMenuBox(
+            modifier = Modifier.padding(0.dp,5.dp),
+            expanded = expanded,
+            onExpandedChange = {
+                expanded = !expanded
+            }
+        ) {
+            TextField(
+                modifier = Modifier.menuAnchor().width(175.dp),
+                value = selectedItem,
+                onValueChange = {},
+                readOnly = true,
+                label = { Text(text = "Sort by:") },
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(
+                        expanded = expanded
+                    )
+                },
+            )
+
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                listItems.forEach { selectedOption ->
+                    DropdownMenuItem(
+                        text = { Text(text = selectedOption) },
+                        onClick = {
+                            selectedItem = selectedOption
+                            expanded = false
+                        }
+                    )
+                }
+            }
+        }
+
+        SegmentedControl(
+            items = titles,
+            defaultSelectedItemIndex = 0
+        ) {
+            selectedOrder = titles[it]
+        }
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -132,7 +203,7 @@ private fun CustomDatePickerDialog(
     displayDatePickerState: DatePickerState,
     closeDialog: () -> Unit
 ) {
-    val datePickerState = rememberDatePickerState(System.currentTimeMillis())
+    val datePickerState = rememberDatePickerState(displayDatePickerState.selectedDateMillis)
     DatePickerDialog(
         onDismissRequest = { closeDialog() },
         confirmButton = {
@@ -192,6 +263,12 @@ private fun CustomDatePickerDialogPreview() {
 @Composable
 private fun CalendarScreenContentPreview() {
     CalendarScreenContent()
+}
+
+@Preview
+@Composable
+private fun SortExercisesScreenPreview() {
+    SortExercisesScreen()
 }
 
 @SuppressLint("SimpleDateFormat") //we provide format as a parameter
